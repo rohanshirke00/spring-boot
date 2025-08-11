c5b8d4d9e695af7ab365d966bacc4cd078ee8b62
Rohan Shirke <rohanshirke25812@gmail.com>
2025-08-11 22:06:30 +0530

    Create Product API

--- product/src/main/java/com/spring_boot/product/controller/ProductController.java
+++ product/src/main/java/com/spring_boot/product/controller/ProductController.java
@@ -1,16 +1,30 @@
 package com.spring_boot.product.controller;
 
 
+import com.spring_boot.product.dto.ProductDTO;
+import com.spring_boot.product.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
+@AllArgsConstructor
 public class ProductController {
+
+    private ProductService productService;
 
     // get all products
 
     // create product
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
+        return new ResponseEntity<>(productService.createProduct(productDTO),HttpStatus.CREATED);
+    }
 
     // get single product
 
--- product/src/main/java/com/spring_boot/product/service/ProductService.java
+++ product/src/main/java/com/spring_boot/product/service/ProductService.java
@@ -1,10 +1,46 @@
 package com.spring_boot.product.service;
 
+import com.spring_boot.product.dto.ProductDTO;
+import com.spring_boot.product.entity.Category;
+import com.spring_boot.product.entity.Product;
+import com.spring_boot.product.mapper.ProductMapper;
+import com.spring_boot.product.repository.CategoryRepository;
+import com.spring_boot.product.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ProductService {
 
+    private ProductRepository productRepository;
+    private CategoryRepository categoryRepository;
 
+    // create a product
+    public ProductDTO createProduct(ProductDTO productDTO){
+        // from payload we get name, description, price, categoryId
+        // now check whether category exist or not using categoryId
+
+        Category category = categoryRepository.findById(productDTO.getCategoryId()).orElseThrow(()-> new RuntimeException("Category not found!"));
+
+        // DTO -> Entity
+        Product product = ProductMapper.toProductEntity(productDTO, category);
+
+        // save the database
+        product = productRepository.save(product);
+
+        // Entity -> DTO
+        return ProductMapper.toProductDTO(product);
+
+    }
+
+
+    // get all products
+
+    // get a product by id
+
+    // update a product
+
+    // delete a product
 
 }
